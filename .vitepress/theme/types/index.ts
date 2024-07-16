7a650efc913c1062c75c16cbe2f19f140ecf0e3d
F-loat <chaimaoyuan@foxmail.com>
2024-07-16 15:16:16 +0800

    feat: support complex types

--- .vitepress/theme/types/index.ts
+++ .vitepress/theme/types/index.ts
@@ -1,15 +1,23 @@
-export interface Data {
-  type:
-    | 'string'
-    | 'integer'
-    | 'long'
-    | 'number'
-    | 'boolean'
-    | 'object'
-    | 'array'
-    | 'null'
-    | 'file'
+interface Common {
   description?: string
   required?: boolean
   default?: string
 }
+
+interface BaseData extends Common {
+  type: 'string' | 'integer' | 'long' | 'number' | 'boolean' | 'null' | 'file'
+}
+
+export interface ObjectData extends Common {
+  type: 'object'
+  properties: {
+    [key: string]: Data
+  }
+}
+
+export interface ArrayData extends Common {
+  type: 'array'
+  items: Data
+}
+
+export type Data = BaseData | ObjectData | ArrayData
